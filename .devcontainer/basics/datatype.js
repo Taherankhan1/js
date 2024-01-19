1432efc20f4fe2c962ce16d3a2cf60c6f4cb58e9
Taherankhan <pathantaheran1122@gmail.com>
2024-01-19 07:50:05 +0000

    datatypes

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer verison
+
+//alert(3+3)// we are using node js not brwoser 
+
+let name="taheran"
+let age=23
+let isLoggedIn=false
+let state;
+
+
+/*
+number
+bigint 
+string=""
+boolean=> true/false
+null =>standalone value 
+undefined 
+symbol=>unique
+
+objects 
+
+
+*/
+console.log(typeof undefined);
