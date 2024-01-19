b9c135d1ecbf0d2c65fb10f3306d75493205e656
Taherankhan <pathantaheran1122@gmail.com>
2024-01-19 07:31:40 +0000

    variables

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="pathantaheran1122@gmail.com"
+var accountPassword="1234"
+accountCity="kadi"
+let accountState
+
+//accountId=2
+accountEmail="abc@gmail.com"
+accountPassword="212121212"
+accountCity="kalol"
+/*
+ prefer not to use var 
+ beacause of issue in block scope and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
