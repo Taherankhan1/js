8020f1cd47cbecfdf48481bde2e4c75a18cdd00b
Taherankhan <pathantaheran1122@gmail.com>
2024-01-22 05:46:55 +0000

    strings

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,10 @@
+let score ="3abc3"
+
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber)); 
+console.log(valueInNumber);
+
+//"33" =>33
+// "33abc"=> NaN
+//ture=>1;false=>0
---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,7 @@
+const name ="taherankhan"
+const age=50
+
+//console.log(name + age + "value");  //outdated 
+
+// in modern programming  
+console.log(`hello my name is ${name} and my age is ${age}`);
