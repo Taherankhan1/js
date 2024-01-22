3a933f6affd3f21fc4c21ef49363d2f8a82810d1
Taherankhan <pathantaheran1122@gmail.com>
2024-01-22 06:14:32 +0000

    strings

--- .devcontainer/basics/strings.js
+++ .devcontainer/basics/strings.js
@@ -5,3 +5,27 @@ const age=50
 
 // in modern programming  
 console.log(`hello my name is ${name} and my age is ${age}`);
+
+const gameName = new String ('taheran')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('3'));
+// console.log(gameName.indexOf('a'));
+ 
+// const newgamename=gameName.substring(0,7)
+
+// console.log(newgamename);
+// const anotherstring = gameName.slice(-7,3)
+// console.log(anotherstring);
+
+// const newstringOne="  taheran  "
+// console.log(newstringOne);
+// console.log(newstringOne.trim());
+
+const url ="https://taheran.com/pathan%20taherankhan"
+console.log(url.replace('%20','-'));
+ 
