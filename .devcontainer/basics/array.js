6b343b4d7935bc895ada397b3537270ec08c1c66
Taherankhan <pathantaheran1122@gmail.com>
2024-01-22 07:48:02 +0000

    array

---
+++ .devcontainer/basics/array.js
@@ -0,0 +1,15 @@
+const myarray= new Array(1,2,3,4)
+console.log(myarray);
+
+//array method 
+
+myarray.push(5)
+console.log(myarray);
+
+myarray.unshift(8);
+console.log(myarray);
+
+myarray.pop();
+console.log(myarray);
+
+console.log(myarray.includes(4));
