e92579761c6eadde347fb7fdd6401add4588ce43
Taherankhan <pathantaheran1122@gmail.com>
2024-01-23 10:12:34 +0000

    foreach

--- .devcontainer/basics/foreach.js
+++ .devcontainer/basics/foreach.js
@@ -15,21 +15,41 @@ const coding = ["java","cpp","python","php"]
 // } )
 
 
-const myCoding = [
-    {
-        name:"javascript",
-        file:"js"
-    },
-    {
-        name:"java",
-        file:"java"
-    },
-    {   
-        name:"python",
-         file:"py"
-    }
-]
-myCoding.forEach( (item) => {
-console.log(item.name);
-console.log(item.file);
-})
+// const myCoding = [
+//     {
+//         name:"javascript",
+//         file:"js"
+//     },
+//     {
+//         name:"java",
+//         file:"java"
+//     },
+//     {   
+//         name:"python",
+//          file:"py"
+//     }
+// ]
+// myCoding.forEach( (item) => {
+// console.log(item.name);
+// console.log(item.file);
+// })
+
+
+//const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+// const newnums=myNums.filter( (num) => {
+//     return num>4
+// } )
+
+
+
+// const newnums=[]
+// myNums.forEach( (num) =>{
+//     if(num>4)
+//     {
+//         newnums.push(num)
+//     }
+// })
+
+// console.log(newnums);
+ 
